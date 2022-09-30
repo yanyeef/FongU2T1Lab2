b130716714a43406605a1ff03f283f0775eac4ea
Yan Yee Fong <yanyeef@nycstudents.net>
2022-09-30 08:28:31 -0400

    initial

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+
+    // instance variables
+    private final double radius;
+
+    // constructor
+    public Circle(double rad) {
+        radius = rad;
+    }
+
+    // method to print area
+    public void printArea() {
+        double area = 3.14 * radius * radius;
+        System.out.println("My area is: " +  area);
+    }
+}
---
+++ src/CircleRunner.java
@@ -0,0 +1,17 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(5.0);
+
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        // the area of the circle
+
+        // D. run the code and confirm your prediction! Were you correct?
+        //  yes i was correct
+    }
+}
